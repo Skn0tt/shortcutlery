2094292986a0fef15f979f45e8ef1cbe7144253f
Simon Knott <info@simonknott.de>
2020-07-01 10:07:07 +0200

    make UI switch appropriately

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import { saveAs } from "file-saver";
 import { createApp } from "./AppBuilder";
+import CommandSvg from "./command.svg";
+import LinkSvg from "./link_icon.svg";
+
+const actionSelect = document.getElementById("action_select") as HTMLSelectElement;
+const specifics = document.getElementById("specifics") as HTMLDivElement;
+const specificsIcon = document.getElementById("specifics-icon") as HTMLImageElement;
+const actionInput = document.getElementById("action_input") as HTMLInputElement;
+
+function getSelectedAction() {
+  return actionSelect.value as "url" | "shell";
+}
+actionSelect.onchange = () => {
+  const specificsText = getSelectedAction() == "shell" ? "which command?" : "which website?";
+  const placeholder = getSelectedAction() == "shell" ? "/usr/bin/code ~/dev/" : "https://simonknott.de";
+  const specificsImg = getSelectedAction() == "shell" ? CommandSvg : LinkSvg;
+
+  specifics.lastChild.textContent = specificsText;
+  specificsIcon.src = specificsImg;
+  actionInput.placeholder = placeholder;
+}
 
 const nameInput = document.getElementById("name_input") as HTMLInputElement;
-console.log(nameInput)
 const spotlightDiv = document.getElementById("spotlight") as HTMLDivElement;
 
 nameInput.onkeyup = () => {
